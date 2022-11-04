1dea6e8974603fc11a974f2b54aa81190bc248dc
ASIX2Hecar <asix2.m14@gmail.com>
2022-11-04 20:30:10 +0100

    commit 1

--- entradaText2pdf.php
+++ entradaText2pdf.php
@@ -5,15 +5,15 @@
     //
     // Accedint al framework
     require_once 'vendor/autoload.php'; //path relatiu al directori a on està el codi principal del projecte 
-    use Dompdf\Dompdf as Dompdf //Utilització del framework Dompdf. Es crea una classe de nom PDF
+    use Dompdf\Dompdf as Dompdf; //Utilització del framework Dompdf. Es crea una classe de nom PDF
 	//
 	// Utilitzant la classe Dompdf del framework. $dompdf serà un objecte de la classe Dompdf
-    dompdf = new Dompdf(); 
+    $dompdf = new Dompdf(); 
     //
     //Convertint text en HTML
     $text=explode("\n",$_GET["text"]);
     $textPDF="";
-    foreac($text as $posicio => $linia) {
+    foreach($text as $posicio => $linia) {
 		$textPDF = $textPDF.htmlspecialchars($linia)."<br>";
 	}
     $dompdf->loadHtml($textPDF);  
@@ -22,7 +22,7 @@
     // 
     $dompdf->setPaper('A4', 'landscape'); //Sets the paper size & orientation
     $dompdf->render(); // Renders the HTML to PDF
-    $dompdf-stream("text_entrada.pdf"); //Streams the PDF to the client (for example: browser)
+    $dompdf->stream("text_entrada.pdf"); //Streams the PDF to the client (for example: browser)
 
 
 ?>
